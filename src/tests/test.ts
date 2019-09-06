a51214cbb8fabbf33a53926bb471539da5f9d900
Tom Van Eyck <tom.vaneyck@telenet.be>
2019-09-06 15:43:29 +0200

    First testing tryout

---
+++ src/tests/test.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import 'mocha';
+
+describe('Meshnetwork', () => {
+    it("should return hello world", () => {
+        const result = "hello world";
+        expect(result).to.equal("hello world");
+    });
+});
